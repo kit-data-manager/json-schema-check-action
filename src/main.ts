20a731cf03a1c543ab8d050b5acc16ea03706e09
Thomas Jejkal <thomas.jejkal@kit.edu>
2025-06-05 14:27:18 +0200

    Initial commit, added basic input handling, currently testing validation and user feedback

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,74 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import { diffString } from 'json-diff'
+import Ajv from 'ajv/dist/2020'
+import * as fs from 'fs'
 
-/**
- * The main function for the action.
- *
- * @returns Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const schemaPath: string = core.getInput('schemaPath')
+    const validate: boolean = core.getBooleanInput('validate')
+    const diff: boolean = core.getBooleanInput('createDiff')
+    // @ts-expect-error Says not constructable, but works.
+    const ajv = new Ajv()
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    let message: string =
+      '# JSON Schema Check Results\n' +
+      '\n' +
+      '### Validate JSON Schema\n' +
+      '\n'
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const data: string = fs.readFileSync(schemaPath, 'utf8')
+    const schema = JSON.parse(data)
+    console.log(schema)
+    let validationErrors: boolean = false
+    if (validate) {
+      const succeed = ajv.validateSchema(schema, false)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+      if (succeed) {
+        message += ':white_check_mark: The schema is valid JSON.\n'
+      } else {
+        message +=
+          ':x: Validation of the schema failed!\n' + '```json\n' + JSON.stringify(ajv.errors) + '\n```\n'
+      }
+    } else {
+      message +=
+        ':grey_question: No information available as validation was not configured. \n'
+      validationErrors = false
+    }
+
+    message += '\n' + '### Create Diff to Latest Release\n' + '\n'
+
+    if (diff) {
+      message +=
+        '```diff\n' +
+        '{\n' +
+        '-  return 1 + 2\n' +
+        '+  return num1 + num2\n' +
+        '}\n' +
+        '```\n' +
+        '\n'
+    } else {
+      message +=
+        ':grey_question: No information available as diff creation was not configured. \n'
+    }
+
+    message += '\n' + '### Next Steps\n' + '\n'
+
+    if (validationErrors) {
+      message += '- [ ] Fix validation errors\n'
+    } else {
+      message += '- [X] Fix validation errors\n'
+    }
+
+    if (diff) {
+      message += '- [ ] Check backwards compatibility based on diff\n'
+    } else {
+      message += '- [X] Check backwards compatibility based on diff\n'
+    }
+    message += '- [ ] React with :thumbsup: to mark the PR as ready'
+
+    core.setOutput('message', message)
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
